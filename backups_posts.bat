ffa172b43c13b575171941403a010ed34c38b298
zzy_desktop <1481635832@qq.com>
2023-08-16 11:37:01 +0800

    更新标签index

--- backups_posts.bat
+++ backups_posts.bat
@@ -1,9 +1,9 @@
 
 
-将文章备份至C:\Users\14816\Documents\笔记文档\_posts
+将文章备份至C:\Users\14816\Documents\NoteDocument\_posts
 并将_posts中的img文件复制至source下的img
 
 cd source
-xcopy _posts C:\Users\14816\Documents\笔记文档\_posts\ /Y 
+xcopy _posts C:\Users\14816\Documents\NoteDocument\_posts\ /Y 
 
-xcopy _posts\img img /Y 
+xcopy img   C:\Users\14816\Documents\NoteDocument\_posts\img\ /Y 
--- source/tags/index.md
+++ source/tags/index.md
@@ -2,6 +2,4 @@
 title: 标签
 date: 2023-06-30 14:09:43
 type: "tags"
-# orderby: random
-# order: 1
 ---
